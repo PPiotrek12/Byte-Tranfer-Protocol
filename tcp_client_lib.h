35869528996135ee6c7f107b03ded9462e12aec1
Piotr Plywacz <piotrus.plywacz@gmail.com>
2024-04-11 16:20:08 +0200

    Correct .h client files

--- tcp_client_lib.h
+++ tcp_client_lib.h
@@ -1,1 +1,6 @@
+#ifndef TCP_CLIENT_LIB_H
+#define TPC_CLIENT_LIB_H
+
 void tcp_client(struct sockaddr_in server_address, char *data, uint64_t seq_len);
+
+#endif // TCP_CLIENT_LIB_H
--- udp_client_lib.h
+++ udp_client_lib.h
@@ -1,1 +1,6 @@
+#ifndef UDP_CLIENT_LIB_H
+#define UDP_CLIENT_LIB_H
+
 void udp_client(struct sockaddr_in server_address, char *data, uint64_t seq_len, uint8_t prot);
+
+#endif // UDP_CLIENT_LIB_H
