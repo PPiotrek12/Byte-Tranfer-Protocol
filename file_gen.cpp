f24cea7a6c2d86f0956084596979d25348617bc8
Piotr Plywacz <piotrus.plywacz@gmail.com>
2024-04-18 17:20:38 +0200

    Test .gitignore 2

--- file_gen.cpp
+++ file_gen.cpp
@@ -2,11 +2,11 @@
 #include <random>
 #include <ctime>
 using namespace std;
-const long long SIZE = 10000000;
+const long long SIZE = 1000000;
 int main() {
     srand(time(NULL));
     for (long long i = 0; i < SIZE; i++) {
-        char c = rand() % 255;
+        char c = rand() % 254 + 1;
         cout.write(&c, 1);
     }
 }
